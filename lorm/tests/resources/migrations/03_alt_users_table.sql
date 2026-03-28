8b119681908f12a92f4f7c8d4dbd18cf0d80a515
MisterPine <git@mr-pine.de>
2026-03-28 10:24:38 +0100

    test: Add field with rename attribute to test

--- lorm/tests/resources/migrations/03_alt_users_table.sql
+++ lorm/tests/resources/migrations/03_alt_users_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE alt_users
 (
     id         INTEGER PRIMARY KEY NOT NULL,
-    email      VARCHAR          NOT NULL UNIQUE,
+    e_mail     VARCHAR             NOT NULL UNIQUE,
     count      INTEGER,
-    created_at DATETIME         NOT NULL DEFAULT(DATETIME('now')),
-    updated_at DATETIME         NOT NULL
+    created_at DATETIME            NOT NULL DEFAULT (DATETIME('now')),
+    updated_at DATETIME            NOT NULL
 );
