c7693e099d08a32b8938f805231e12ad9fe4a965
Rémy Saissy <remy.saissy@gmail.com>
2026-04-30 01:28:15 +0200

    fix(tests): make MySQL role_name indexable

--- lorm/tests/resources/migrations/mysql/08_user_roles_named_table.sql
+++ lorm/tests/resources/migrations/mysql/08_user_roles_named_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS user_roles_named (
     user_id BINARY(16) NOT NULL,
-    role_name TEXT NOT NULL,
+    role_name VARCHAR(255) NOT NULL,
     PRIMARY KEY (user_id, role_name)
 );
